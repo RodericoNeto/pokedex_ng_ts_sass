93c810243bfafae21d501f892e13bde449739a95
RodericoNeto <rodericocrneto@gmail.com>
2023-06-08 17:14:37 -0300

    feat: pokemon data

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from '../../_model/Pokemon'
+import { Type } from '../../_model/Type'
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
+  @Input()
+  public pokemon!: Pokemon;
+
+  public leadingZero(str: string | number, size:number=3): string{
+    let s = String(str);
+    while(s.length < (size || 2)){s = "0" + s;}
+    return s;
+  }
 
 }
